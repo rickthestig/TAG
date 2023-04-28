642783d45cb769b2cdae6b458d8e0ba60d6b23da
rickthestig <erekg13@gmail.com>
2023-04-27 19:09:25 -0500

    keywords may be working

--- Search Faculty.php
+++ Search Faculty.php
@@ -19,49 +19,50 @@
         Student <input type="text" id="sname" name="sname"><br>
         //TODO: add dropdown for archived or not
         <div class="checkbox-grid">
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Algorithms</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">CMS</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Communication</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Contest</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Database</label>
+            <label><input type="checkbox" name="checkbox[]"  value="1">Algorithms</label>
+            <label><input type="checkbox" name="checkbox[]"  value="2">CMS</label>
+            <label><input type="checkbox" name="checkbox[]"  value="3">Communication</label>
+            <label><input type="checkbox" name="checkbox[]"  value="4">Contest</label>
+            <label><input type="checkbox" name="checkbox[]"  value="5">Database</label>
 
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">e-Commerce</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Education</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Events</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">External</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">External - For Profit</label>
+            <label><input type="checkbox" name="checkbox[]"  value="6">e-Commerce</label>
+            <label><input type="checkbox" name="checkbox[]"  value="7">Education</label>
+            <label><input type="checkbox" name="checkbox[]"  value="8">Events</label>
+            <label><input type="checkbox" name="checkbox[]"  value="9">External</label>
+            <label><input type="checkbox" name="checkbox[]"  value="10">External - For Profit</label>
 
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">File Storage</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Internal</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">KMS</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Marketing</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Networks</label>
+            <label><input type="checkbox" name="checkbox[]"  value="11">File Storage</label>
+            <label><input type="checkbox" name="checkbox[]"  value="12">Internal</label>
+            <label><input type="checkbox" name="checkbox[]"  value="13">KMS</label>
+            <label><input type="checkbox" name="checkbox[]"  value="14">Marketing</label>
+            <label><input type="checkbox" name="checkbox[]"  value="15">Networks</label>
 
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Open Source</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Procedures</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Programming</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Queries</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Resources</label>
+            <label><input type="checkbox" name="checkbox[]"  value="16">Open Source</label>
+            <label><input type="checkbox" name="checkbox[]"  value="17">Procedures</label>
+            <label><input type="checkbox" name="checkbox[]"  value="18">Programming</label>
+            <label><input type="checkbox" name="checkbox[]"  value="19">Queries</label>
+            <label><input type="checkbox" name="checkbox[]"  value="20">Resources</label>
 
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Security</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Social</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Web</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Website</label>
-            <label><input type="checkbox" name="checkbox[]" id="/" value="/">Workshop</label>
+            <label><input type="checkbox" name="checkbox[]"  value="21">Security</label>
+            <label><input type="checkbox" name="checkbox[]"  value="22">Social</label>
+            <label><input type="checkbox" name="checkbox[]"  value="23">Web</label>
+            <label><input type="checkbox" name="checkbox[]"  value="24">Website</label>
+            <label><input type="checkbox" name="checkbox[]"  value="25">Workshop</label>
         </div>
         <input type="submit">
     </form>
     <?php
-    $client=$_REQUEST["client"];
-    $name=$_REQUEST["PName"];
-    $year=$_REQUEST["year"];
-    $course=$_REQUEST["course"];
-    $student=$_REQUEST["sname"];
+    $client=$_POST["client"];
+    $name=$_POST["PName"];
+    $year=$_POST["year"];
+    $course=$_POST["course"];
+    $student=$_POST["sname"];
+    $checkbox=$_POST["checkbox"];
     // need checkbox request as well
     $conn = new mysqli("localhost", "kmkelmo1", "vYV7v[66(kX9lD", "kmkelmo1_capstone_database");
     //main search is proj name
     if ($name != null) {
-        $query = "SELECT ProjectName,ProjectDescription,Course,ProjectYear,Archive,Client.ClientName,Student.FirstName,Student.LastName FROM Project JOIN ProjectAndClient ON Project.ProjectID = ProjectAndClient.ProjectID JOIN Client ON ProjectAndClient.ClientID = Client.ClientID JOIN ProjectAndStudent ON Project.ProjectID = ProjectAndStudent.ProjectID JOIN Student ON ProjectAndStudent.StudentID = Student.StudentID WHERE ProjectName LIKE %$name%";
+        $query = "SELECT ProjectName,ProjectDescription,Course,ProjectYear,Archive,Client.ClientName,Student.FirstName,Student.LastName, Keyword.Keyword FROM Project JOIN ProjectAndClient ON Project.ProjectID = ProjectAndClient.ProjectID JOIN Client ON ProjectAndClient.ClientID = Client.ClientID JOIN ProjectAndStudent ON Project.ProjectID = ProjectAndStudent.ProjectID JOIN Student ON ProjectAndStudent.StudentID = Student.StudentID JOIN ProjectAndKeyword ON ProjectAndKeyword.ProjectID = Project.ProjectID JOIN Keyword ON Keyword.KeywordID = ProjectAndKeyword.KeywordID WHERE ProjectName LIKE %$name%";
         //ADD KEYWORDS
     }
     if($client != null){
@@ -74,14 +75,26 @@
         $query = $query . " AND Course LIKE %$course%";
     }
     if($name != null){
-        $query = $query . " AND Student.FirstName LIKE %$name% OR Student.LastName LIKE %$name%";
+        $query = $query . " AND Student.FirstName LIKE %$sname% OR Student.LastName LIKE %$sname%";
     }
     echo $query;
-    foreach(checkbox[] as $checkboxres){
-        if(!is_null($checkboxres)){
+    foreach($checkbox as $checkboxname=>$checkboxvalue){
+        if(!is_null($checkboxvalue)){
+            $keywordquery = "SELECT ProjectID,KeywordID FROM ProjectAndKeyword WHERE KeywordID LIKE %$checkboxvalue%";
+            $stmt = $conn->prepare($keywordquery);
+            $stmt->execute();
+            $stmt->bind_result($PID,$KID);
+            $bindquery = "SELECT ProjectID FROM Project WHERE ProjectName LIKE %$name%";
+            $stmt = $conn->prepare($bindquery);
+            $stmt->execute();
+            $stmt->bind_result($BPID);
+            if($BPID == $PID){
+                $query = $query . " AND Keyword.KeywordID LIKE %$KID%";
+            }
             //do query to get result for checkbox and return it to the table below
         }
     }
+    $query = $query . ";";
     $stmt = $conn->prepare($query);
         $stmt->execute();
         $stmt->bind_result($col1,$col2,$col3,$col4,$col5);
